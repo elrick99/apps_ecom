67c15b8b5f976bfba76ea2013f3b0a05e251d8c9
elrick99 <61994062+elrick99@users.noreply.github.com>
2020-08-20 12:44:41 +0000

    commit 20-08-2020 12:44

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:apps_ecom/Providers/Models/Product.dart';
 import 'package:apps_ecom/Providers/Services/Categories.dart';
 import 'package:apps_ecom/Providers/Services/Products.dart';
 import 'package:apps_ecom/Screens/Details_Screen.dart';
+import 'package:apps_ecom/Screens/Profil_Screen.dart';
 import 'package:apps_ecom/Widgets/Article_Home.dart';
 import 'package:apps_ecom/Widgets/BottomBar.dart';
 import 'package:flutter/material.dart';
@@ -26,12 +27,20 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
+        themeMode: ThemeMode.system,
+
         theme: ThemeData(
+          iconTheme: IconThemeData(
+            color: Color(0xFFee7b77),
+          ),
+          appBarTheme: AppBarTheme(
+            color: Colors.white,
+          ),
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         // home: BottomBar(),
-        home: ArticleScreen(),
+        home: ProfilScreen(),
         routes: {'/detail': (context) => DetailsScreen()},
       ),
     );
